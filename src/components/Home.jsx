79a5ee7a191f7109ebda76b422289e694eb94ec8
Clara Thuo <clarathuo0@gmail.com>
2024-08-08 02:20:56 +0300

    updates new file

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import React, { useState } from 'react'
+
+function Home() {
+  return (
+    const [transactions, setTransactions] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+  )
+}
