bb2b00df36a582a6e338a0d5fdb58a946a7b6460
Clara Thuo <clarathuo0@gmail.com>
2024-08-07 11:06:21 +0300

    Update added file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TransactionTable from './components/TransactionTable';
 function App() {
- 
   return(
+    <div className="App">
+      <h1>Transactions App</h1>
+      <TransactionTable />
+    </div>
   )
 }
 
---
+++ src/components/TransactionTable.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import TransactionRow from './TransactionRow';
+
+//render table header
+
+function TransactionTable({transactions}) {
+  return (
+    <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            {transactions.map((transaction) => (
+                <TransactionRow key={transaction.id} transaction={transaction} />
+            ))}
+        </tbody>
+    </table>
+  )
+}
+export default TransactionTable;
